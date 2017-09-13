91e5e54d5f0809af3df7250d4b2490dd47bbc95a
Amer00100 <amridris@gmail.com>
2017-09-12 19:20:22 -0700

    Third commit

--- bigPosInteger.cpp
+++ bigPosInteger.cpp
@@ -28,7 +28,7 @@ bigPosInteger::bigPosInteger(std::string value)
 
 
 bigPosInteger::bigPosInteger(int value)
-/*this instructor takes in an integer and constructs it into a bigPosInteger type*/
+/*this Constructor takes in an integer and constructs it into a bigPosInteger type*/
 {
     string value1;
     value1 = to_string(value);
@@ -57,25 +57,25 @@ bigPosInteger::bigPosInteger(const bigPosInteger& value)
 /*This is a copy constructor, be EXTREMELY careful for memory leaks here*/
 {
     //Since Length is not a pointer, we shallow copy the content to length.
-    length = value.length;
+    this ->length = value.length;
 
     //IF statement to check and see if the object value has data"numbers"
     if(value.valueArray)
     {
         //If there are numbers in value object, we create a new block memory to save it
-        valueArray = new int[length];
+        this ->valueArray = new int[length];
 
         //for loop to copy the content of value object to the new object "Deep Copy"
         for(int i=0; i<length;++i)
         {
-            valueArray[i]= value.valueArray[i];
+            this->valueArray[i]= value.valueArray[i];
         }
 
     }
     //If value is empty, the valueArray object will be donated as zero
     else{
 
-        valueArray = 0;
+        this->valueArray = 0;
     }
 
 
@@ -94,67 +94,116 @@ bigPosInteger bigPosInteger::operator+ (const bigPosInteger& rhs)
     //create a new object to save the result
     bigPosInteger result;
     long long temp;
-    long long size;
-    int carry;
+    int carry=0;
 
-    //Compare the sizes of the two operand using if statement
-    if(this->length>rhs.length)
+    //Carry is always 0 at the beginning of the addition
+    //if this-> length is greater than rhs.length, then result.valueArray size will equal to this->length + for any carry over.
+    if(this->length > rhs.length)
     {
-        //if "this" is greater than rhs, it becomes the primary operand and the new object size will be set size of this object +1 for the carry
         result.length = this->length+1;
-        //loop through the values and add
-        for(size = result.length; size>=0; size--)
-        {
-            //create a temp memory to hold the value
-            temp = this->valueArray[size]+rhs.valueArray[size]+carry;
-
-            //check to see if the addition produce a carry
-            if(temp>=10)
-            {
-                temp = temp %10;
-                carry = 1;
-                result.valueArray[size] = temp;
-            }
-            else{
-                result.valueArray[size] = temp;
-                carry = 0;
-            }
-        }
+        result.valueArray = new int[result.length];
 
     }
+    // else we just assign the size of result.valueArray to the size of rhs.valueArray+1 for the carry.
     else {
-        //Repeat the above process but we are setting the length to rhs.length
-        result.length = rhs.length;
-        for(size = result.length; size>=0; size--)
-        {
-            temp = rhs.valueArray[size]+rhs.valueArray[size]+carry;
-        }
+        result.length = rhs.length+1;
+        result.valueArray = new int [result.length];
+    }
+    //Loop through the array to start the addition
+    for(int i= result.length;i>=result.length;i--)
+    {
+        //Add the values from the two arrays and save it in temp
+        temp = this->valueArray[i]+rhs.valueArray[i]+carry;
+        //if temp is greater than 10, we have a carry
         if(temp>=10)
         {
-            temp = temp %10;
+            // result will be temp value - 10
+            result.valueArray[i] = temp%10;
+            //Carry will be set to one
             carry = 1;
-            result.valueArray[size] = temp;
         }
-
         else{
-            result.valueArray[size]=temp;
+            //just add the two values
+            result.valueArray[i] = temp;
+            //since the values are not bigger than 10, carry will remain 0
             carry = 0;
         }
-
     }
+
+//return the object result
     return result;
 }
 
 bigPosInteger bigPosInteger::operator- (const bigPosInteger& rhs)
 /*this operator should be able to subtract the Right Hand Side bigPosInteger from the base bigPosInteger and return the result. The default return should be replaced with the appropriate variable*/
 {
-    return bigPosInteger(std::__cxx11::basic_string<char, std::char_traits<char>, std::allocator<char>>());
+    bigPosInteger result;
+    long long temp;
+    int borrow;
+
+    //if this->length is greater than rhs.length, result.valueArray size will be equal to this->length.
+    if(this->length> rhs.length)
+    {
+        result.length = this->length;
+        result.valueArray = new int [result.length];
+        //Initialize the result.valueArray to zero
+        for(int i=0; i<=result.length; i++){
+            result.valueArray[i] = 0;
+        }
+    }
+    //Else the size of result.valueArray will be set to rhs.length
+    else {
+        result.length = rhs.length;
+        result.valueArray = new int[result.length];
+        //Initialize the result.valueArray to zero
+        for(int i=0; i<=result.length; i++){
+            result.valueArray[i] = 0;
+        }
+    }
+
+    borrow = 0;
+    //borrow at the beginning of subtraction is always 0 zero
+    for(int i = result.length; i>=0; i--)
+    {
+        //If there was no borrow, compare the numbers
+        if(borrow == 0)
+        {
+            //if the first operand is greater the second operand then simply subtract
+            if(this->valueArray[i]>=rhs.valueArray[i])
+            {
+                result.valueArray[i] = this->valueArray[i] - rhs.valueArray[i];
+            }
+            //If the second operand is greater then 2nd operand, borrow from the next digit and add 10: A-B+10
+            else {
+                result.valueArray[i] = this->valueArray[i]+10-rhs.valueArray[i];
+                borrow = 1;
+
+            }
+        }
+        //Here borrow = 1, therefore we deduct the "1" value from the next subtraction
+        else {
+            //Compare the number and if the first operand is greater than second simply subtract and also deduct the borrow value.
+            if(this->valueArray[i]-1>=rhs.valueArray[i])
+            {
+                result.valueArray[i] = this->valueArray[i]-rhs.valueArray[i]-1;
+                borrow = 0;
+            }
+            // if the 2nd digit is greater than the first, then add 10 and deduct from the borrow: A+B-1+10 or A+B+9
+            else{
+                result.valueArray[i]= this->valueArray[i]-rhs.valueArray[i]+9;
+                borrow = 1;
+            }
+        }
+    }
+    return result;
 }
 
 bigPosInteger bigPosInteger::operator*(const bigPosInteger& rhs)
 /*this operator should be able to multiply two bigPosInteger together and return the result. The default return should be replaced with the appropriate variable*/
 {
-    return bigPosInteger(std::__cxx11::basic_string<char, std::char_traits<char>, std::allocator<char>>());
+    bigPosInteger result;
+    int i, j;
+    long long temp, 
 }
 
 bigPosInteger &bigPosInteger::operator=(const bigPosInteger& rhs)
